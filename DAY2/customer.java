d5128ee32888507bf7021f58d3ae5aaccf5fda19
harivarshannn <harivarshan2006@outlook.com>
2024-10-27 13:56:24 +0530

    modified customer.java

--- DAY2/customer.java
+++ DAY2/customer.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class CustomerSurvey {
+public class customer {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         boolean isShoppingFun;
