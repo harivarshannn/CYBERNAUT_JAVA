b837d83f9642aa6b950cb67ac99872f13b489855
harivarshannn <harivarshan2006@outlook.com>
2024-10-27 13:57:11 +0530

    modified shopping.java

---
+++ DAY2/shopping.java
@@ -0,0 +1,7 @@
+public class Shopping {
+    public static void main(String[] args) {
+        double itemPrice;
+        itemPrice = 19.99;
+        System.out.println("The price of the item in the shopping cart is: $" + itemPrice);
+    }
+}
